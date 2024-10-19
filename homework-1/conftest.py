ae01dc8ee80112d88d8c28f65d1096b2fd1a9e32
Lika <lika@gmail.com>
2024-10-19 21:39:38 +0300

    python at
    - homework 1

---
+++ homework-1/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+import yaml
+import requests
+from lxml.html.diff import token
+from Post import Post
+from Connection import Connection
+
+connection = Connection("config.yaml")
+
+@pytest.fixture
+def login():
+    res1 = requests.post(
+        url=connection.host + "gateway/login",
+        data={"username":connection.username, "password": connection.password}
+    )
+    print(res1.content)
+    return res1.json()["token"]
+
+@pytest.fixture
+def testtext1():
+    return "Test post 10"
+
+
+@pytest.fixture
+def new_post():
+    return Post("config.yaml")
+
---
+++ homework-1/test_tack2.py
@@ -0,0 +1,18 @@
+import requests
+
+from Connection import Connection
+
+connection = Connection("config.yaml")
+
+def test_step1(login, testtext1):
+    header = {"X-Auth-Token": login}
+    res = requests.get(connection.host+"api/posts", params={"owner":"notMe"}, headers=header)
+    lisres = [i["title"] for i in res.json()["data"]]
+    assert testtext1 in lisres
+
+def test_create_post(login, new_post):
+    x_auth_token = {"X-Auth-Token": login}
+    res = requests.post(connection.host + "api/posts", headers=x_auth_token, data=new_post.payload())
+    postId = res.json()["id"]
+    res = requests.get(connection.host + "api/posts/" + str(postId), headers=x_auth_token)
+    assert res.status_code == 200
